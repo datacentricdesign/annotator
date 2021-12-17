471e50eb8a6ad15d6a84f9b2baa0982307554f9c
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-12-17 13:39:20 +0100

    try with runsever

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+# python manage.py migrate --no-input
+# python manage.py collectstatic --no-input
 
-gunicorn annotator.wsgi:application --bind 0.0.0.0:80
+# gunicorn annotator.wsgi:application --bind 0.0.0.0:80
+
+python manage.py runserver
