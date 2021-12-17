4d0d164c11051487ce8b439a07b568b98f9539fa
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-12-17 18:01:18 +0100

    debug bool

--- annotator/settings.py
+++ annotator/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", 'False') == 'True'
+DEBUG = env("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'annotator','storytelling.io.tudelft.nl']
 
