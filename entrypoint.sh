1b20181d241bdb990e8ed511b8dcc74160926cc9
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-12-17 13:37:56 +0100

    remove double 00

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn annotator.wsgi:application --bind 0.0.0.0:8000
+gunicorn annotator.wsgi:application --bind 0.0.0.0:80
