e626cb9da743518364321b9c05c52d5a2732315f
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-12-17 15:51:54 +0100

    try back with docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# python manage.py migrate --no-input
-# python manage.py collectstatic --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
-# gunicorn annotator.wsgi:application --bind 0.0.0.0:80
-
-python manage.py runserver
+gunicorn annotator.wsgi:application --bind 0.0.0.0:8000
