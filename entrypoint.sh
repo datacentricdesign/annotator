cfdab1a6eed65aeeb3cbc1c274e92e631df4eaf9
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2021-12-17 15:54:04 +0100

    echo messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+echo "Migrating..."
 python manage.py migrate --no-input
+echo "Making static files..."
 python manage.py collectstatic --no-input
 
+echo "Starting Gunicorn..."
 gunicorn annotator.wsgi:application --bind 0.0.0.0:8000
